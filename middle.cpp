9eb7680c8ca152e026ac3444bbe5041f51bdfbc3
rockstarchik <112154161+rockstarchik@users.noreply.github.com>
2022-11-02 14:57:54 +0300

    Add files via upload

---
+++ middle.cpp
@@ -0,0 +1,50 @@
+#include "middle.h"
+using namespace std;
+
+ void itc_num_print(int number){
+cout << number;
+ }
+
+ int itc_len_num(long long number)
+ {
+     int col;
+     col = 0;
+     while (number > 0) {
+        col=col+1;
+        number=number/10;
+     }
+     return col;
+ }
+
+ int itc_sum_num(long long number){
+     int sum;
+     sum = 0;
+     while(number > 0){
+        sum=number%10+sum;
+     number=number/10;
+     }
+ return sum;
+ }
+
+ long long itc_multi_num(long long number){
+ int pro;
+ pro = 1;
+  while(number > 0){
+    pro=pro * number%10;
+  number=number/10;
+ }
+ return pro;
+ }
+
+ int itc_max_num(long long number){
+ int m, n;
+ m=0;
+ while(number > 0){
+        n=number%10;
+    if(n > m)
+        m = n;
+    number=number/10;
+ }
+ return m;
+ }
+
---
+++ middle.h
@@ -0,0 +1,10 @@
+#ifndef MIDDLE_H_INCLUDED
+#define MIDDLE_H_INCLUDED
+#include <iostream>
+
+void itc_num_print(int number);
+int itc_len_num(long long number);
+int itc_sum_num(long long number);
+long long itc_multi_num(long long number);
+int itc_max_num(long long number);
+#endif // MIDDLE_H_INCLUDED

73e13c823340542c83898b7feaf17758884004fe
rockstarchik <112154161+rockstarchik@users.noreply.github.com>
2022-11-02 14:58:39 +0300

    Update middle.h

--- middle.h
+++ middle.h
@@ -7,4 +7,10 @@ int itc_len_num(long long number);
 int itc_sum_num(long long number);
 long long itc_multi_num(long long number);
 int itc_max_num(long long number);
+int itc_min_num(long long number);
+int itc_rev_num(long long number);
+int itc_null_count(long long number);
+bool itc_mirror_num(long long number);
+int itc_mirror_cout(long long number);
+
 #endif // MIDDLE_H_INCLUDED

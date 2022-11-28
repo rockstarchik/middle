44b75d3438ddb2e53f4ef26571e0195eaa543bfd
rockstarchik <112154161+rockstarchik@users.noreply.github.com>
2022-11-28 14:07:16 +0300

    Add files via upload

--- middle.h
+++ middle.h
@@ -15,5 +15,7 @@ int itc_mirror_cout(long long number);
 int itc_second_max_num(long long number);
 long long itc_bin_num(long long number);
 long long itc_oct_num(long long number);
+int itc_rev_bin_num(long long number);
+int itc_rev_oct_num(long long number);
 
 #endif // MIDDLE_H_INCLUDED#pragma once
